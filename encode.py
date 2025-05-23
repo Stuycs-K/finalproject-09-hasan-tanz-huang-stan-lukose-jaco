1d3487fb43c292b34beb9d67b4af2f672f75a4c3
Tanzeem Hasan <thasan50@stuy.edu>
2025-05-23 13:47:43 -0500

    Devlog

--- encode.py
+++ encode.py
@@ -1,7 +1,10 @@
-# Rotate letter by a certain caesar shift per rotor
-def rotorShift(str, charRotate):
+# Rotate letter by a certain caesar shift for a given rotor, then move to the next rotor and cycle through
+def rotorShift(str, charRotors):
     shifted = ""
     for i in range(len(str)):
-        shifted += char(ord(str[i]) + ord(charRotate))
+        shifted += char(ord(str[i]) + ord(charRotate[i%3]))
     return shifted
-def plugboard(str):
+
+# Swap letters
+def plugboard(str, swapSettings):
+    # Get letters to swap with each other from the user
