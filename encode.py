6f3111014208de73b4a670fb3105167ad5590587
Stanley huang <shuang50@stuy.edu>
2025-05-24 13:14:31 -0400

    working first 2 rotors going backwards

--- encode.py
+++ encode.py
@@ -1,31 +1,45 @@
 def index(char):
     return (ord(char) - ord('A')) % 26
-
+def find_integer(integer, array):
+    for index, i in enumerate(array):
+        print(str(i) + " and " + str(chr(ord('A') + integer)))
+        if (integer == i):
+            return index
+    print("you're dumb")
+    return(-1)
 def encode(char, plugboard, rotors):
+    #setup
     old0 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
     old1 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
     old2 = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+    reflect = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
     r0 = []
     r1 = []
     r2 = []
+    reflector = []
     for i in range(len(old0)):
         r0.append(index(old0[i]))
         r1.append(index(old1[i]))
         r2.append(index(old2[i]))
+        reflector.append(index(reflect[i]))
+    #going forward
     char = index(char)
-    print("char index is " + str(r2[(char + index(rotors[2]))] % 26))
     char += r2[(char + index(rotors[2])) % 26] - index(rotors[2])
     char %= 26
-    print("char is" + str(char)) 
-    "C"
-    print("char index is " + str((r1[char + index(rotors[1])] % 26)))
-    "D"
     char = r1[(char + index(rotors[1])) % 26] - index(rotors[1])
     char %= 26
-    print("char is1  " + str(char))
-    print("char index is " + str(r1[char + index(rotors[1]) % 26]))
     char = r0[(char + index(rotors[0])) % 26] - index(rotors[0])
     char %= 26
+    print(chr(char + ord('A')))
+    #reflect
+    char = reflector[char]
+    print(chr(char + ord('A')))
+    #going backward
+    char = find_integer(char, r0)
+    char = find_integer(char, r1)
+    print("char is after r1 " + str(char))
+    char = find_integer(char, r2)
+
     print(chr(char + ord('A')))
 
 encode('A', [], ['A', 'A', 'C'])
