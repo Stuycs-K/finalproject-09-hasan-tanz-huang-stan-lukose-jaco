e542da7ed4c24e2ef4a5bcb730657280f687e954
Stanley huang <shuang50@stuy.edu>
2025-05-24 13:32:29 -0400

    working encode given set rotor and input

--- encode.py
+++ encode.py
@@ -9,6 +9,9 @@ def find_integer(integer, array):
     return(-1)
 def encode(char, plugboard, rotors):
     #setup
+    rotors[0] = index(rotors[0])
+    rotors[1] = index(rotors[1])
+    rotors[2] = index(rotors[2])
     old0 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
     old1 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
     old2 = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
@@ -24,22 +27,22 @@ def encode(char, plugboard, rotors):
         reflector.append(index(reflect[i]))
     #going forward
     char = index(char)
-    char += r2[(char + index(rotors[2])) % 26] - index(rotors[2])
+    char += r2[(char + rotors[2]) % 26] - rotors[2]
     char %= 26
-    char = r1[(char + index(rotors[1])) % 26] - index(rotors[1])
+    char = r1[(char + rotors[1]) % 26] - rotors[1]
     char %= 26
-    char = r0[(char + index(rotors[0])) % 26] - index(rotors[0])
+    char = r0[(char + rotors[0]) % 26] - rotors[0]
     char %= 26
     print(chr(char + ord('A')))
     #reflect
     char = reflector[char]
     print(chr(char + ord('A')))
     #going backward
-    char = find_integer(char, r0)
-    char = find_integer(char, r1)
+    char = find_integer(char + rotors[0], r0) - rotors[0]
+    char = find_integer(char + rotors[1], r1) - rotors[1]
     print("char is after r1 " + str(char))
-    char = find_integer(char, r2)
+    char = find_integer(char + rotors[2], r2) - rotors[2]
 
     print(chr(char + ord('A')))
 
-encode('A', [], ['A', 'A', 'C'])
+encode('A', [], ['B', 'C', 'H'])
