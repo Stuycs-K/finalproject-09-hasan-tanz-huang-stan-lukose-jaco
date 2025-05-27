fe5fd4683f1b009b93e4549c6845a59e54af29bb
Stanley huang <shuang50@stuy.edu>
2025-05-27 01:15:28 -0400

    working parseargs but bugged speed from last commit

--- encode.py
+++ encode.py
@@ -1,8 +1,10 @@
+import sys
+
 def index(char):
     return (ord(char) - ord('A')) % 26
 #setup
 r = ["EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO"]
-findr = [[0] * 26] * 3
+findr = [[0] * 26, [0] * 26, [0] * 26]
 reflect = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 reflector = []
 for i in range(len(r)):
@@ -13,12 +15,22 @@ for i in range(len(r)):
 for i in range(len(reflect)):
     reflector.append(index(reflect[i]))
 
-print(r[2])
-print(findr[2])
-
+print(r[1])
+print(findr[1])
+def find_integer(integer, array):
+    integer = mod26(integer)
+    for index, i in enumerate(array):
+        #print(str(i) + " and " + str(chr(ord('A') + integer)))
+        if (integer == i):
+            return index
+    print("you're dumb")
+    return(-1)
 def mod26(integer):
     return (integer + 26) % 26
+
 def encode(char, plugboard, rotors):
+    #print(rotors)
+    #print(char + "  ", end="")
     char = index(char)
     #plugboard
     char = pb(char, plugboard)
@@ -29,12 +41,16 @@ def encode(char, plugboard, rotors):
         char %= 26
     #reflect
     char = reflector[char]
+    print("char is " + chr(char + ord('A')))
     #going backward
     for i in range(len(r)):
+        print("the mod is" + str(mod26(char + rotors[i])))
         char = mod26(r[i][findr[i][mod26(char + rotors[i])]] - rotors[i])
+        print("char is " + chr(char + ord('A')))
+
     #plugboard again
     char = pb(char, plugboard)
-    #print(chr(char + ord('A')), end = "")
+    print(chr(char + ord('A')), end = "")
 
 def pb(char, plugboard):
     for i in plugboard:
@@ -45,7 +61,6 @@ def pb(char, plugboard):
                 return i[len(i) - j - 1]
     return char
 
-
 def update(rotors):
     if (rotors[2] == index('V')):
         rotors[1] = mod26(rotors[1] + 1)
@@ -53,17 +68,59 @@ def update(rotors):
             rotors[0] = mod26(rotors[0] + 1)
     rotors[2] = mod26(rotors[2] + 1)
 
-def enigma(plugboard, rotors):
-    string = "the sky is blue and the grass is green. jokic deserves to win mvp"
+def parseArgs():
+    values = []
+    try:
+        plugboard = sys.argv[1]
+        rotors = sys.argv[2]
+    except IndexError:
+        print("Invalid number of arguments")
+        return None
+    plugboard = plugboard.upper().strip()
+    rotors = rotors.upper().strip()
+    if (len(plugboard) != 0):
+        try:
+            plugboard1 = plugboard.split(' ')
+        except ValueError:
+            print("Plugboard must be pairs of letters that never repeat separated by a single space in between")
+            return None
+        plugboard2 = list(plugboard)
+        for i in range(len(plugboard2)):
+            if ((i % 3 == 2 and plugboard2[i] != ' ') or (i % 3 != 2 and (not (ord(plugboard2[i]) >= ord('A') and ord(plugboard2[i]) <= ord('Z')) or plugboard2[i] in plugboard2[:i]))):
+                print("Plugboard must be pairs of letters that never repeat separated by a single space in between")
+                return None
+        plugboard = [list(joe) for joe in plugboard1]
+    else:
+        plugboard = []
+    if (len(rotors) != 3):
+        print("Rotor settings must have length 3")
+        return None
+    rotors = list(rotors)
+    for i in range(len(rotors)):
+        if (not(ord(rotors[i]) >= ord('A') and ord(rotors[i]) <= ord('Z'))):
+            print("Rotor settings must be 3 consecutive letters")
+            return(None)
+    return [plugboard, rotors]
+    
+
+def enigma():
+    arguments = parseArgs()
+    if (arguments is None):
+        return None
+    plugboard = arguments[0]
+    rotors = arguments[1]
+    string = "A"
     for i in range(len(plugboard)):
         for j in range(len(plugboard[i])):
             plugboard[i][j] = index(plugboard[i][j])
     for i in range(len(rotors)):
         rotors[i] = index(rotors[i])
+    print("plugboard is" + str(plugboard))
+    print("rotors is " + str(rotors))
     for i in range(len(string)):
         if ((ord(string[i]) >= ord('A') and ord(string[i]) <= ord('Z')) or (ord(string[i]) >= ord('a') and ord(string[i]) <= ord('z'))):
             update(rotors)
             encode(string[i].upper(), plugboard, rotors)
-    #print("")
+    print("")
 
-enigma([['A', 'Z'], ['B', 'Y'], ['C', 'X'], ['D', 'W'], ['E', 'V'], ['F', 'U'], ['G', 'T'], ['H', 'S'], ['I', 'R'], ['J', 'Q']], ['A', 'A', 'A'])
+enigma()
